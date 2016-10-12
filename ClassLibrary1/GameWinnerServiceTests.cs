b11b1452ef00b6ba8eca6f3187a77482fa966e37
jespe <jespe@DESKTOP-RE9P93D>
2016-10-12 13:25:12 +0200

    Add project files.

---
+++ ClassLibrary1/GameWinnerServiceTests.cs
@@ -0,0 +1,127 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToe.Services;
+
+namespace TicTacToe.UnitTests
+{
+    [TestFixture]
+    public class GameWinnerServiceTests
+    {
+        private GameWinnerService _gameWinnerService;
+        private char[,] _gameBoard;
+        private int Teller;
+        const char expected = 'X';
+        [SetUp]
+        public void SetupUnitTests() {
+            _gameWinnerService = new GameWinnerService();
+            _gameBoard = new char[3, 3] {
+                    {' ', ' ', ' '},
+                    {' ', ' ', ' '},
+                    {' ', ' ', ' '}
+                };
+        }
+        [Test]
+        public void NeitherPlayerHasThreeInARow()
+        {
+
+            const char expected = ' ';
+            var actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void PlayerWithAllSpacesInTopRowIsWinner()
+        {
+            IGameWinnerService gameWinnerService;
+            gameWinnerService = new GameWinnerService();
+            var gameBoard = new char[3, 3] {
+                    { expected, expected, expected},
+                    {' ', ' ', ' '},
+                    {' ', ' ', ' '}
+                };
+            var actual = gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+        [Test]
+        public void PlayerWithAllSpacesInMiddelRowIsWinner()
+        {
+            IGameWinnerService gameWinnerService;
+            gameWinnerService = new GameWinnerService();
+            var gameBoard = new char[3, 3] {
+                    {' ', ' ', ' '},
+                    {expected, expected, expected},
+                    {' ', ' ', ' '}
+                };
+            var actual = gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+        [Test]
+        public void PlayerWithAllSpacesInBottomRowIsWinner()
+        {
+            IGameWinnerService gameWinnerService;
+            gameWinnerService = new GameWinnerService();
+            var gameBoard = new char[3, 3] {
+                    {' ', ' ', ' '},
+                    {' ', ' ', ' '},
+                    { expected, expected, expected }
+                };
+            var actual = gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+        [Test]
+        public void PlayerWithAllSpacesInFirstColumnIsWinner()
+        {
+            for (var columnIndex = 0; columnIndex < 3; columnIndex++)
+            {
+                _gameBoard[columnIndex, 0] = expected;
+            } var actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+        [Test]
+        public void PlayerWithAllSpacesInSecondColumnIsWinner()
+        {
+            for (var columnIndex = 0; columnIndex < 3; columnIndex++)
+            {
+                _gameBoard[columnIndex, 1] = expected;
+            }
+            var actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+        [Test]
+        public void PlayerWithAllSpacesInThirdColumnIsWinner()
+        {
+            for (var columnIndex = 0; columnIndex < 3; columnIndex++)
+            {
+                _gameBoard[columnIndex, 2] = expected;
+            }
+            var actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+        [Test]
+        public void PlayerWithThreeInARowDiagonallyDownAndToRightIsWinner()
+        {
+            for (var cellIndex = 0; cellIndex < 3; cellIndex++)
+            {
+                _gameBoard[cellIndex, cellIndex] = expected;
+            } var actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+        [Test]
+        public void PlayerWithThreeInARowDiagonallyDownAndToLftIsWinner()
+        {
+            Teller = 2;
+            for (var cellIndex = 0; cellIndex < 3; cellIndex++)
+            {
+                _gameBoard[cellIndex, Teller - cellIndex] = expected;
+
+            }
+            var actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+
+    }
+
+}
---
+++ ClassLibrary2/GameWinnerService.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Services
+{
+
+
+    public class GameWinnerService : IGameWinnerService
+    {
+        private const char SymbolForNoWinner = ' ';
+        private char cellOneChar;
+        private char cellTwoChar;
+        private char cellThreeChar;
+        private char cellFourChar;
+        private char cellFiveChar;
+        private char cellSixChar;
+        private char cellSevenChar;
+        private char cellEightChar;
+        private char cellNineChar;
+        public char Validate(char[,] gameBoard)
+        {
+            setCells(gameBoard);
+            
+            var currentWinningSymbol = CheckForThreeInARowInHorizontalRow();
+            if (currentWinningSymbol != SymbolForNoWinner) return currentWinningSymbol;
+            currentWinningSymbol = CheckForThreeInARowInVerticalColumn();
+            if (currentWinningSymbol != SymbolForNoWinner) return currentWinningSymbol;
+            currentWinningSymbol = CheckForThreeInARowDiagonally();
+            return currentWinningSymbol;
+            
+        }
+        private char CheckForThreeInARowInVerticalColumn()
+        {
+
+            if (cellOneChar == cellFourChar && cellFourChar == cellSevenChar && cellOneChar != ' ')
+            {
+                return cellOneChar;
+            }
+            else if (cellTwoChar == cellFiveChar && cellFiveChar == cellEightChar && cellTwoChar != ' ')
+            {
+                return cellTwoChar;
+            }
+            else if (cellThreeChar == cellSixChar && cellSixChar == cellNineChar && cellThreeChar != ' ')
+            {
+                return cellThreeChar;
+            }
+            else
+            {
+                return SymbolForNoWinner;
+            }
+        }
+        private char CheckForThreeInARowDiagonally()
+        {
+
+            if (cellOneChar == cellFiveChar && cellFiveChar == cellNineChar && cellOneChar != ' ')
+            {
+                return cellOneChar;
+            }
+            else if (cellThreeChar == cellFiveChar && cellFiveChar == cellSevenChar && cellThreeChar != ' ')
+            {
+                return cellThreeChar;
+            }
+            return SymbolForNoWinner;
+        }
+        private char CheckForThreeInARowInHorizontalRow()
+        {
+
+            if (cellOneChar == cellTwoChar && cellTwoChar == cellThreeChar && cellOneChar != ' ')
+            {
+                return cellOneChar;
+            }
+            else if (cellFourChar == cellFiveChar && cellFiveChar == cellSixChar && cellFourChar != ' ')
+            {
+                return cellFourChar;
+            }
+            else if (cellSevenChar == cellEightChar && cellEightChar == cellNineChar && cellSevenChar != ' ')
+            {
+                return cellSevenChar;
+            }
+            return ' ';
+        }
+        private void setCells(char[,] gameBoard)
+        {
+            cellOneChar = gameBoard[0, 0];
+            cellTwoChar = gameBoard[0, 1];
+            cellThreeChar = gameBoard[0, 2];
+            cellFourChar = gameBoard[1, 0];
+            cellFiveChar = gameBoard[1, 1];
+            cellSixChar = gameBoard[1, 2];
+            cellSevenChar = gameBoard[2, 0];
+            cellEightChar = gameBoard[2, 1];
+            cellNineChar = gameBoard[2, 2];
+        }
+
+    }
+}
